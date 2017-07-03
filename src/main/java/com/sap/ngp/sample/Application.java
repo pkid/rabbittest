0bf345a12f6650999f7ea46bff9536e1f83fb86f
root <root@vagrant.vm>
2017-07-03 13:47:05 +0000

    test

--- src/main/java/com/sap/ngp/sample/Application.java
+++ src/main/java/com/sap/ngp/sample/Application.java
@@ -1,14 +1,46 @@
 package com.sap.ngp.sample;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import java.nio.charset.Charset;
+
+import org.apache.commons.codec.binary.Base64;
+
 
 @SpringBootApplication
 
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        //SpringApplication.run(Application.class, args);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String auth = "guest:guest";
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")) );
+        String authHeader = "Basic " + new String( encodedAuth );
+        headers.set( "Authorization", authHeader );
+
+        String uri = "http://localhost:15672/api/exchanges/%2f/my-new-exchange-new";
+        String uri2 = "http://localhost:15672/api/exchanges";
+
+
+        String input = "{\"type\":\"direct\",\"durable\":\"true\"}";
+
+        HttpEntity<String> entity = new HttpEntity<String>(input,headers);
+        System.out.println(entity);
+
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.PUT, entity, String.class);
+
+        System.out.println(response);
+
     }
 }
